963f28c5c3d4ed6a529c9484d297bce99db1da50
mdono <mdono@intecap.edu.gt>
2022-08-04 21:31:05 -0600

    hola mundo

---
+++ 01_HolaMundo/src/main/java/Manuel.java
@@ -0,0 +1,13 @@
+// este es un comentario de una sola línea
+// quiero seguir comentando
+/*
+todo lo que este dentro
+de este espacio
+se considera un comentario
+*/
+
+public class Manuel {
+    public static void main(String args[]){
+        System.out.println("Hola Mundo");
+    }
+}
